50f722ff4ff88fc687a84268f0879502904c3e98
Aditya Tile <adityatile99@gmail.com>
2025-08-14 19:53:00 +0530

    add basic maven structure

---
+++ maven/my.app/src/main/java/com/aditya/App.java
@@ -0,0 +1,17 @@
+package com.aditya;
+
+import com.aditya.Helper.Helper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hello From maven");
+        Helper.help();
+    }
+}
---
+++ maven/my.app/src/main/java/com/aditya/Helper/Helper.java
@@ -0,0 +1,7 @@
+package com.aditya.Helper;
+
+public class Helper {
+    public static void help() {
+        System.out.println("Helping method");
+    }
+}
