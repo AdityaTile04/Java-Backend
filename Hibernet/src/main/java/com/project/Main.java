94a1a5c401a74fdf3e6c357aafac7c32c0d94f91
Aditya Tile <adityatile99@gmail.com>
2025-08-16 23:51:18 +0530

    started learning about hibernate

---
+++ Hibernet/src/main/java/com/project/Main.java
@@ -0,0 +1,40 @@
+package com.project;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+       Student s = new Student();
+       s.setId( 102 );
+       s.setName( "Rushi" );
+       s.setTech( "Gen AI" );
+
+        Configuration config = new Configuration();
+        config.addAnnotatedClass( com.project.Student.class);
+        config.configure();
+
+        SessionFactory factory = config.buildSessionFactory();
+        Session session = factory.openSession();
+
+//        Student s1 = session.get( Student.class, 102 );
+//        Student s1 = session.byId( Student.class ).getReference( 102 ); // lazy loading
+//        Student s1 = session.find( Student.class , 101); // Eager fetching
+//        System.out.println(s1);
+
+        Transaction transaction = session.beginTransaction();
+        // updation
+        session.merge( s );
+
+        Student s2 = session.find( Student.class, 102 );
+        session.remove( s2 );
+
+        session.persist( s );
+
+//        session.persist( s );
+        transaction.commit();
+
+
+    }
+}
