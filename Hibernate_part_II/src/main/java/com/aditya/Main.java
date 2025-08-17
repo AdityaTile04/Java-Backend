13e32682ff53ea1b0841614b6b73405ad6114a7f
Aditya Tile <adityatile99@gmail.com>
2025-08-17 23:10:34 +0530

    refactoring the code

--- Hibernate_part_II/src/main/java/com/aditya/Main.java
+++ Hibernate_part_II/src/main/java/com/aditya/Main.java
@@ -9,25 +9,34 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Employee e = new Employee();
-        e.setId( 103 );
-        e.setName("Kunal");
-        e.setEmail( "Kunal@gmail.com" );
-        e.setSalary(65000);
-        System.out.println(e);
+        e.setId( 108 );
+        e.setName("rohit");
+        e.setEmail( "rohit@gmail.com" );
+        e.setSalary(30000);
 
-        Configuration config = new Configuration();
-        config.addAnnotatedClass( com.aditya.Employee.class );
-        config.configure("hibernate.cfg.xml");
 
-        SessionFactory sessionFactory = config.buildSessionFactory();
+//        Configuration config = new Configuration();
+//        config.addAnnotatedClass( com.aditya.Employee.class );
+//        config.configure("hibernate.cfg.xml");
+
+
+         // Inserting Data
+        SessionFactory sessionFactory = new Configuration()
+                .addAnnotatedClass( com.aditya.Employee.class )
+                .configure()
+                .buildSessionFactory();
+
         Session session = sessionFactory.openSession();
 
         Transaction transaction = session.beginTransaction();
 
         session.persist( e );
+        System.out.println(e);
+
         transaction.commit();
 
-
+        session.close();
+        sessionFactory.close();
 
 
     }
