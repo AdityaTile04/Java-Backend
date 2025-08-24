6cc395344cf8b55f457bbedb36030274a68d920c
Aditya Tile <adityatile99@gmail.com>
2025-08-24 12:52:29 +0530

    started learning spring core with xml-based configuration

---
+++ spring_app_II/spring_app/src/main/java/com/aditya/Laptop.java
@@ -0,0 +1,7 @@
+package com.aditya;
+
+public class Laptop {
+    public void compile() {
+        System.out.println("compiling....");
+    }
+}
---
+++ spring_app_II/spring_app/src/main/java/com/aditya/Student.java
@@ -0,0 +1,7 @@
+package com.aditya;
+
+public class Student {
+    public void printName() {
+        System.out.println("Aditya Tile");
+    }
+}
