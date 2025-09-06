d0b05985b9675ff42a9dea0aeed9f0a9129397e2
Aditya Tile <adityatile99@gmail.com>
2025-09-06 17:31:54 +0530

    Learned about Aspect Oriented Programming

---
+++ Spring_boot/spring-aop/src/main/java/com/aditya/spring_boot_rest/aop/LoggingAspect.java
@@ -0,0 +1,21 @@
+package com.aditya.spring_boot_rest.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( LoggingAspect.class );
+
+    @Before( "execution(*  com.aditya.spring_boot_rest.service.JobService.getJob(..)) || execution(*  com.aditya.spring_boot_rest.service.JobService.updateJob(..))" )
+    public void logMethodCall(JoinPoint jp) {
+        LOGGER.info( "Method called " + jp.getSignature().getName() );
+    }
+
+}
