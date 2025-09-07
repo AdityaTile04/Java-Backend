1c5b665f599963ebe1a85252e40f8d96ebe94f88
Aditya Tile <adityatile99@gmail.com>
2025-09-07 21:32:50 +0530

    used Customizer for CSRF and request authorization in security config

--- Spring_boot/spring_security/src/main/java/com/project/spring_security/config/SecurityConfig.java
+++ Spring_boot/spring_security/src/main/java/com/project/spring_security/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,11 +17,31 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(customizer  -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        http.formLogin( Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session -> session.sessionCreationPolicy( SessionCreationPolicy.STATELESS ));
+        Customizer<CsrfConfigurer<HttpSecurity>> customizeCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+                configurer.disable();
+            }
+        };
+
+        http.csrf(customizeCsrf);
+
+        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> customizeHttp = new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+            @Override
+            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+                registry.anyRequest();
+            }
+        };
+
+        http.authorizeHttpRequests(customizeHttp);
+
+
+            http
+                .csrf(customizer  -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+//              .formLogin( Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy( SessionCreationPolicy.STATELESS ));
 
         return http.build();
     }
