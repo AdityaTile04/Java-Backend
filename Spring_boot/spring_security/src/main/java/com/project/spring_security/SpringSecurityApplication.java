c10311bd2b2464ac21f27c0ae59ce2a498bd5a52
Aditya Tile <adityatile99@gmail.com>
2025-09-07 12:53:15 +0530

    setup spring security project

---
+++ Spring_boot/spring_security/src/main/java/com/project/spring_security/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.project.spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringSecurityApplication.class, args );
+        System.out.println("Application started");
+    }
+
+}
