dd3fad3e8ae58755e63894c642506726118b9d7b
Aditya Tile <adityatile99@gmail.com>
2025-09-12 16:24:53 +0530

    Add login endpoint and configure Spring Security with stateless auth

--- Spring_boot/spring_security/src/main/java/com/project/spring_security/controller/UserController.java
+++ Spring_boot/spring_security/src/main/java/com/project/spring_security/controller/UserController.java
@@ -3,8 +3,10 @@ package com.project.spring_security.controller;
 import com.project.spring_security.model.User;
 import com.project.spring_security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,27 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    private AuthenticationManager manager;
+
     @PostMapping("register")
     public User register(@RequestBody User user) {
         user.setPassword( encoder.encode( user.getPassword() ) );
         System.out.println(user.getPassword());
         return service.saveUser( user );
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user) {
+
+        Authentication authentication = manager
+                .authenticate( new UsernamePasswordAuthenticationToken( user.getUsername(), user.getPassword() ) );
+
+        if(authentication.isAuthenticated())
+            return "Login Successful";
+        else
+            return "Login Failed";
+
     }
 
 }
