ea73a20e0120b44806021298ef1033944c32a83e
Aditya Tile <adityatile99@gmail.com>
2025-09-13 14:08:33 +0530

    Add Google and GitHub OAuth2 authentication integration

---
+++ Spring_boot/spring_OAuth2/src/main/java/com/project/spring_oauth2/SpringOAuth2Application.java
@@ -0,0 +1,14 @@
+package com.project.spring_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringOAuth2Application.class, args );
+        System.out.println("Application Started");
+    }
+
+}
