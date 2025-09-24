b60667634d8aae44ebd46eab666986cf658b476f
Aditya Tile <adityatile99@gmail.com>
2025-09-24 16:05:07 +0530

    Set up Docker Compose for Spring Boot app and Postgres

--- Docker/spring-database/src/main/java/com/project/springdatabase/controller/StudentController.java
+++ Docker/spring-database/src/main/java/com/project/springdatabase/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.project.springdatabase.model.Student;
 import com.project.springdatabase.repository.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +19,14 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getAllStudents() {
         return repo.findAll();
+    }
+
+    @RequestMapping("/addStudent")
+    public void addStudent() {
+        Student student = new Student();
+        student.setName( "Nikhil" );
+        student.setMarks( 88 );
+        repo.save( student );
     }
 
 }
--- Docker/spring-database/src/main/java/com/project/springdatabase/model/Student.java
+++ Docker/spring-database/src/main/java/com/project/springdatabase/model/Student.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
@@ -19,12 +17,27 @@ public class Student {
     private String name;
     private int marks;
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", marks=" + marks +
-                '}';
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
     }
 }
